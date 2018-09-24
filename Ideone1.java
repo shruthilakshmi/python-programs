bdffd5c8bac6519789b38cfc0f14103e7e08c05b
shruthilakshmi <42768597+shruthilakshmi@users.noreply.github.com>
2018-09-24 14:25:41 +0530

    Update Ideone1.java

--- Ideone1.java
+++ Ideone1.java
@@ -2,25 +2,18 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
 class Ideone
 { 
 	public static void main(String[] args) 
 	{
-
         Scanner reader = new Scanner(System.in);
-
         System.out.print("Enter a number: ");
         int num = reader.nextInt();
-
         if(num % 2 == 0)
             System.out.println("Even");
         else if(num%3==0) 
             System.out.println("Odd");
-              else 
-              
+              else    
             System.out.println("Invalid");
-    
 	}
     }
