24145f966f758bb204e68bcbb66f39e1cef8a478
shruthilakshmi <42768597+shruthilakshmi@users.noreply.github.com>
2018-09-24 13:42:29 +0530

    Create Ideone.java

---
+++ Ideone.java
@@ -0,0 +1,24 @@
+/*positive or negative*/ 
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		      int num;
+        //Scanner class to read value
+        Scanner sc=new Scanner(System.in);
+        num=sc.nextInt();
+         
+        //check condition for +ve, -ve and Zero
+        if(num>0)
+            System.out.println("Positive");
+        else if(num<0)
+            System.out.println("Negative");
+        else
+            System.out.println("Zero");
+    }
+}
